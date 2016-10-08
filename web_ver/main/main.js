67fbba5b2d3292f7eec79c38fbb96546ad7b9d53
Yuki Morikubo <infinitymyskilife@gmail.com>
2016-10-08 23:56:45 +0900

    upload web_ver

---
+++ web_ver/main/main.js
@@ -0,0 +1,19 @@
+
+
+function push(btnNo){
+if (btnNo == 1){
+   location.href = "../measure/measure.html";
+}else if(btnNo == 2){
+   location.href = "../tryon/tryon.html";
+}
+}
+
+    
+function selectheight(){
+  var a = 1;
+    alert(a);
+}
+
+function selectclothes(){
+    
+}
