2c7007579bb9ffede2af0d007591ed6c756118ff
Yuki Morikubo <infinitymyskilife@gmail.com>
2016-10-23 01:26:08 +0900

    サイズ感判定、値渡し

--- web_ver/main.js
+++ web_ver/main.js
@@ -1,10 +1,10 @@
-var h = 169;
-var body_shoulder =40;
-var body_chest = 90;
-var body_waist = 80;
+var body_shoulder;
+var body_chest=90;
+var body_waist=70;
 var shoulder_size;
-var chest_size= 10 ;
+var chest_size;
 var waist_size;
+var sizeflag;
 
 <!--チェスト、ウエストは中央値-->
 function Data(shoulder, chest, waist){
@@ -20,23 +20,31 @@ var uniT_m = new Data(43,92,80);
 var uniT_l = new Data(44.5,100,88);
 var uniT_xl = new Data(46.5,108,96);
 
+function Dataset(){
+    var clone = decodeURIComponent(location.search.substring(1));
+    clone = clone.split('&');
+    body_shoulder = clone[0];
+    body_chest = clone[1];
+    body_waist = clona[2];
+}
+
 
 function ClosizeCalclation(clothes_shoulder, clothes_chest, clothes_waist){
+    sizeflag=1;
     shoulder_size = body_shoulder - clothes_shoulder;
     chest_size = body_chest - clothes_chest;
     waist_size = body_waist - clothes_waist;
-    location.href = "../size.html";
 }
 
 function jump_main(){
-    location.href="../main/index.html";
+    location.href="../main/index.html?"+body_shoulder+"&"+body_chest+"&"+body_waist;
 }
 
 function push(btnNo){
 if (btnNo == 1){
    location.href = "../measure/measure.html";
 }else if(btnNo == 2){
-   location.href = "../tryon/tryon.html";
+   location.href = "../tryon/tryon.html?"+location.search.substring(1);
 }else if(btnNo == 3){
    location.href = "men/men.html";
 }else if(btnNo == 4){
@@ -49,15 +57,15 @@ function jump_cursor(){
 }
 
 function jump_uniT(){
-    location.href = "uniT/uniT.html";
+    location.href = "uniT/uniT.html?"+location.search.substring(1);
 }/*tシャツを選択した場合*/
 
 function jump_uniB(){
-	location.href = "uniB/uniB.html";
+	location.href = "uniB/uniB.html?"+location.search.substring(1);
 }/*パンツを選択した場合*/
 
 function jump_check(){
-	location.href = "../../check/check.html";
+	location.href = "../../check/check.html?"+location.search.substring(1);
 }
 
 
